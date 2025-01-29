f80063aa6013df0212461a3624214fae73c1e8a8
mdaffanwd <mohammedaffan2305@gmail.com>
2025-01-29 19:20:22 +0530

    add userModel.js

---
+++ task-manager-backend/src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+// Password Hashing Middleware
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+
+})  
+
+export default mongoose.model("User", userSchema);
